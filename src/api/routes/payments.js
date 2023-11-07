617e4d6ca64c2d3bd618c81afd31bc4ffc96aac9
GusEngers <gustavoengers6441@gmail.com>
2023-11-07 17:00:53 -0300

    Definición de rutas "payments"

--- src/api/routes/payments.js
+++ src/api/routes/payments.js
@@ -1,3 +1,11 @@
 const payments = require('express').Router();
 
+payments.route('/').get(async (req, res) => {
+  try {
+    res.json({ msg: 'Pago efectuado' });
+  } catch (error) {
+    res.json({ msg: 'Pago fallido' });
+  }
+});
+
 module.exports = payments;
