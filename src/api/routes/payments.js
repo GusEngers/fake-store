8082e9a6ba7a2ff0f21989a9ba4bc68d369a0289
GusEngers <gustavoengers6441@gmail.com>
2023-11-07 17:36:43 -0300

    Fix: Cambio de método 'GET' a 'POST' en payments

--- src/api/routes/payments.js
+++ src/api/routes/payments.js
@@ -1,6 +1,6 @@
 const payments = require('express').Router();
 
-payments.route('/').get(async (req, res) => {
+payments.route('/').post(async (req, res) => {
   try {
     res.json({ msg: 'Pago efectuado' });
   } catch (error) {
