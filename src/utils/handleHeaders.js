0b286ee73176bc1133e732adf89f66951ceb2997
GusEngers <gustavoengers6441@gmail.com>
2023-11-15 00:12:24 -0300

    TODO: Mejorar método para identificar si el cliente añade un header 'accept'

--- src/utils/handleHeaders.js
+++ src/utils/handleHeaders.js
@@ -8,19 +8,19 @@ module.exports = (req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, OPTIONS, PATCH');
 
-  const accept = req.get('accept');
+  // const accept = req.get('accept');
 
-  const condition =
-    req.method === 'GET' &&
-    accept !== undefined &&
-    !accept.toLowerCase().includes('application/json');
+  // const condition =
+  //   req.method === 'GET' &&
+  //   accept !== undefined &&
+  //   !accept.toLowerCase().includes('application/json');
 
-  if (condition) {
-    return res.status(406).json({
-      message: 'Not Acceptable',
-      status: 406,
-      errors: [`It will only reply with 'content-type: application/json'`],
-    });
-  }
+  // if (condition) {
+  //   return res.status(406).json({
+  //     message: 'Not Acceptable',
+  //     status: 406,
+  //     errors: [`It will only reply with 'content-type: application/json'`],
+  //   });
+  // }
   next();
 };
